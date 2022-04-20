40f071894a3648875c6a00b67ab65fed2aa94381
Student <volchkov_i@1060.ru>
2022-04-20 14:25:32 +0300

    пм

--- bot.py
+++ bot.py
@@ -3,9 +3,15 @@ from key import TOKEN
 from telegram import Update, ReplyKeyboardMarkup, ReplyKeyboardRemove
 from openpyxl import load_workbook
 from connect_to_database import *
+from telegram.ext import ConversationHandler
 
+bd = load_workbook('database.xlsx')
 
-bd = load_workbook('base.xlsx')
+grades = [
+    ['8', '9', '10', '11'],
+]
+
+WAIT_NAME, WAIT_SEX, WAIT_GRADE = range(3)
 
 
 def main():
@@ -13,134 +19,245 @@ def main():
         token=TOKEN,
         use_context=True
     )
-
+    # диспетчер распределяет сообщения по обработчикам
     dispatcher = updater.dispatcher
 
-    keyboard_handler = MessageHandler(Filters.text('клавиатура'), keyboard)
-    exo_handler = MessageHandler(Filters.all, exo)
-    hallo_handler = MessageHandler(Filters.text('Привет, привет'), say_hallo)
+    # создаём обработчик
+    echo_handler = MessageHandler(Filters.all, do_echo)
+    new_sticker_handler = MessageHandler(Filters.text('Добавить стикер'), new_sticker)
+    text_handler = MessageHandler(Filters.text, meet)
+    hello_handler = MessageHandler(Filters.text('Привет'), say_hello)
+    bye_handler = MessageHandler(Filters.text('пока'), say_bye)
+    keyboard_handler = MessageHandler(Filters.text('Клавиатура, клавиатура'), keyboard)
+    conv_handler = ConversationHandler(
+        entry_points=[text_handler],  # Точка старта
+        states={
+            WAIT_NAME: [MessageHandler(Filters.text, ask_sex)],
+            WAIT_SEX: [MessageHandler(Filters.text, ask_grade)],
+            WAIT_GRADE: [MessageHandler(Filters.text, greet)],
+        },  # Состояния конечного автомата для диалога
+        fallbacks=[],  # общие точки выхода или отмены
+    )
 
-    dispatcher.add_handler(hallo_handler)
+    # регестрируем обработчик
+    dispatcher.add_handler(conv_handler)
+    dispatcher.add_handler(hello_handler)
     dispatcher.add_handler(keyboard_handler)
-    dispatcher.add_handler(exo_handler)
+    dispatcher.add_handler(new_sticker_handler)
+    dispatcher.add_handler(bye_handler)
+    dispatcher.add_handler(echo_handler)
 
     updater.start_polling()
+    print('Бот успешно запустился')
     updater.idle()
 
 
-def say_hallo(update, context):
+def do_echo(update: Update, context: CallbackContext) -> None:
+    id = update.message.chat_id
+    user = update.message.from_user.username
+    text = update.message.text
+    sticker = update.message.sticker
+    if sticker:
+        sticker_id = sticker.file_id
+        update.message.reply_sticker(sticker_id)
+    update.message.reply_text(text=
+                              f' держи своё {text}\n'
+                              f'а это твой имя {id}\n'
+                              f'и вообще ты @{user}\n'
+
+                              )
+
+
+def say_hello(update: Update, context: CallbackContext):
+    name = update.message.from_user.first_name
+    update.message.reply_text(text=f'Привет, {name} \n'
+                                   f' приятно познакомиться с живым человеком\n'
+                                   f'Я - бот'
+                              )
+
+
+def say_bye(update: Update, context: CallbackContext):
+    update.message.reply_sticker(stickers['пока'])
+
+
+def say_smth(update: Update, context: CallbackContext):
     name = update.message.from_user.first_name
     text = update.message.text
-    update.message.reply_text(text="привет")
+    for keyword in stickers:
+        if keyword in text:
+            if stickers[keyword]:
+                update.message.reply_sticker(stickers[keyword])
+            if replies[keyword]:
+                update.message.reply_text(replies[keyword].format(name))
+            break
+    else:
+        do_echo(update, context)
 
 
-def exo(update: Update, context: CallbackContext):
-    name = update.message.from_user.first_name
-    user_id = update.message.chat_id
-    text = update.message.text if update.message.text else "но текста нет"
-    update.message.reply_text(text=f'Привет, {name}!\n'
-                              f'Твой id: {user_id}\n'
-                              f'Ты написал какой-то ****: {text}\n')
+def new_sticker(update: Update, context: CallbackContext) -> None:
+    sticker_id = update.message.sticker.file_id
+    for keyword in stickers:
+        if sticker_id == stickers[keyword]:
+            update.message.reply_text('у меня тоже такой есть')
+            update.message.reply_sticker(sticker_id)
+            break
+    else:
+        context.user_data['new_sticker'] = sticker_id
+        update.message.reply_text('введи ключевое слово')
 
 
-def keyboard(update: Update, context: CallbackContext):
+def new_keyword(update: Update, context: CallbackContext) -> None:
+    if 'new_sticker' not in context.user_data:
+        say_smth(update, context)
+    else:
+        keyword = update.message.text
+        sticker_id = context.user_data['new_sticker']
+        insert_sticker(keyword, sticker_id)
+        context.user_data.clear()
+
+
+def keyboard(update: Update, context: CallbackContext) -> None:
     buttons = [
-        ['1', '2', '3'],
-        ['привет', 'да', 'пока']
+        ['Добавить стикер', '2', '3'],
+        ['Привет', 'Пока']
     ]
-    keys = ReplyKeyboardMarkup
+    keys = ReplyKeyboardMarkup(
+        buttons
+    )
     update.message.reply_text(
-        text='смотри, есть клава ',
+        text='Смотри, у тебя появились кнопки!',
         reply_markup=ReplyKeyboardMarkup(
             buttons,
-            resize_keyboard=True
+            resize_keyboard=True,
+            # one_time_keyboard=True,
 
         )
     )
 
 
 def meet(update: Update, context: CallbackContext):
-    """
-    Старт диалог по добавлению пользователя в БД.
-    Будут собраны последовательно:
+    '''
+    старт диалога по добавлению пользователя в базу данных
+    будут собраны последовательно
+        id пользователя
         имя
         пол
         класс
-        id пользователя
-    """
+    '''
     user_id = update.message.from_user.id
     if in_database(user_id):
-        pass  # выход из диалога
-    ask_name(update, context)
+        return
+    return ask_name(update, context)
 
 
-def ask_name(update, Update, context: CallbackContext):
-    """
-    спрашивает у пользователя имя
-    TODO проверить имя пользователя в телеграмме
-    """
+def ask_name(update: Update, context: CallbackContext):
+    '''
+    спрашиваем имя
+    TODO проверить имя пользователя в телеграме
+    '''
     update.message.reply_text(
-        'ты не в базе данных\n'
-        'введи своё имя'
+        'Привет, меня зовут Бот\n'
+        'А тебя?'
     )
+    return WAIT_NAME
 
 
-def ask_sex(update, Update, context: CallbackContext):
-    """
-     спрашивает у пользователя пол
-    """
+def ask_sex(update: Update, context: CallbackContext):
+    '''
+    спрашиваем пол, выводим клавиатуру
+    '''
     name = update.message.text
-    context.user_data['name'] = name
+    if not name_is_valid(name):
+        update.message.reply_text(
+            'Некорректное имя\n'
+            'Имя должно состоять из букв и начинаться с заглавной\n'
+            'Попробуй ещё раз'
+        )
+        return WAIT_NAME
+    context.user_data['name'] = name  # запоминаем имя
     buttons = [
-        ['М', 'Ж']
+        ['м', 'ж']
     ]
     keys = ReplyKeyboardMarkup(
         buttons,
-        resize_keyboard=True
+        resize_keyboard=True  # размер
     )
-    reply_text = f'приятно познакомится, {name}\n' + 'теперь введи свой пол'
     update.message.reply_text(
-        reply_text,
+        text=f'Приятно познакомиться, {name}, укажи пожалуйста свой пол',
+        reply_markup=keys  # разметка
+    )
+    return WAIT_SEX
+
+
+def ask_grade(update: Update, context: CallbackContext):
+    '''
+    спрашиваем класс с помощью клавиатуры
+    '''
+    sex = update.message.text
+    if not sex_is_valid(sex):
+        update.message.reply_text(
+            'Некорректное пол\n'
+            'Воспользуйся клавиатурой\n'
+            'Попробуй ещё раз'
+        )
+        return WAIT_SEX
+    context.user_data['sex'] = sex
+    keys = ReplyKeyboardMarkup(
+        grades,
+        resize_keyboard=True  # размер
+    )
+    update.message.reply_text(
+        text='Укажи пожалуйста свой класс',
         reply_markup=keys
     )
+    return WAIT_GRADE
 
-    def ask_grade(update, Update, context: CallbackContext):
-        """
-        спрашивает у пользователя класс в котором он учится
-        """
-        sex = update.message.text
-        context.user_data['name'] = sex
-        buttons = [
-            ['10н', '10о', '10н', '10о'],
-            ['я не ученик']
-        ]
-        keys = ReplyKeyboardMarkup(
-            buttons,
-            resize_keyboard=True
-        )
-        reply_text = f''
+
+def greet(update: Update, context: CallbackContext):
+    '''
+    Записывает в БД:
+        user_id(из сообщения)
+        name(из контекста)
+        sex(из контекста)
+        grade(из сообщения)
+    приветствует нового пользователя
+    '''
+    grade = update.message.text
+    if not grade_is_valid(grade):
         update.message.reply_text(
-            reply_text,
-            reply_markup=keys
+            'Некорректный класс\n'
+            'Воспользуйся клавиатурой\n'
+            'Попробуй ещё раз'
         )
-def greet(update, Update, context: CallbackContext):
-    """
-
-
-    """
-    user_id = update.message.from_user.id
+        return WAIT_GRADE
     name = context.user_data['name']
-    sex = context.user_data['data']
-    grate = update.message.text
+    sex = context.user_data['sex']
+    user_id = update.message.from_user.id
+    insert_user(user_id, name, sex, grade)
 
-    # TODO нужна функция записи всего
-    update.message.reply_text(  # ответ на этапе отладки
-       f'Новая запись в БД\n'
-       f'{user_id}\n'
-       f'{name}\n'
-       f'{sex}\n'
-       f'{grate}\n'
-     )
+    update.message.reply_text(
+        f'Новая запись в БД\n'
+        f'{user_id}\n'
+        f'{name}\n'
+        f'{sex}\n'
+        f'{grade}'
+    )
+    return ConversationHandler.END
+
+
+def name_is_valid(name: str) -> bool:
+    return name.isalpha() and name[0].isupper() and name[1:].islower()
+
+
+def sex_is_valid(sex: str) -> bool:
+    return sex == 'м' or sex == 'ж'
+
+
+def grade_is_valid(grade: str) -> bool:
+    for row in grades:
+        if grade in row:
+            return True
+        return False
 
 
 if __name__ == '__main__':
--- connect_to_database.py
+++ connect_to_database.py
@@ -1,19 +1,57 @@
 from openpyxl import load_workbook
 
 
-def insert_sticker(keyword):
-    pass
+def insert_sticker(keyword, sticker_id=None, reply_text=None):
+    row = stickers_page.max_row + 1
+    stickers_page.cell(row=row, column=1).value = keyword
+    stickers_page.cell(row=row, column=2).value = sticker_id
+    stickers_page.cell(row=row, column=3).value = reply_text
+    bd.save('database.xlsx')
+
+    stickers[keyword] = sticker_id
+    replies[keyword] = reply_text
 
 
 def in_database(user: int) -> bool:
     """
-    Возвращает True, если пользователь user есть в базе данных...
+    возвращает True, если id пользователь есть в database
     """
-    bd = {}
-    users_page = bd['Users']
-    return True
+
+    for row in range(1, user_page.max_row + 1):
+        if user == user_page.cell(row=row, column=1).value:
+            return True
+        return False
 
 
+def insert_user(*args):  # принимает произвольное количество аргументов
+    """
+    вносит нового пользователя в базу данных
+    """
+    row = user_page.max_row + 1
+    user_page.cell(row=row, column=1).value = args[0]
+    user_page.cell(row=row, column=2).value = args[1]
+    user_page.cell(row=row, column=3).value = args[2]
+    user_page.cell(row=row, column=4).value = args[3]
+    bd.save('database.xlsx')
 
 
+bd = load_workbook('database.xlsx')
+for sheet in bd:
+    print(sheet.title)
+stickers_page = bd['stickers']
+user_page = bd['users']
 
+stickers = {}
+replies = {}
+
+for row in range(1, stickers_page.max_row + 1):
+    keyword = stickers_page.cell(row=row, column=1).value
+    sticker_id = stickers_page.cell(row=row, column=2).value
+    reply_text = stickers_page.cell(row=row, column=3).value
+    stickers[keyword] = sticker_id
+    replies[keyword] = reply_text
+
+
+if __name__ == '__main__':
+    insert_user(12345, 'yasrat', 'ж', '10 o')
+    print(stickers)
